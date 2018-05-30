cce827163f661934d609979ad8143e2ecabb9c60
ikaserg <ikaserg@gmail.com>
2018-05-30 22:36:27 +0300

    1

--- smm/ok_bot/post_status.py
+++ smm/ok_bot/post_status.py
@@ -7,7 +7,7 @@ def main():
     db = get_db_connect()
     w = OkWalker(db, '89106455257')
     w.login()
-
+    #develop
     w.post_status('Нужен совет опытного юриста? Я решу ваши проблемы! Звоните 8 915 598-92-07')
     sleep(30)
 main()
